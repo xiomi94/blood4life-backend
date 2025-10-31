eae907b8451456402b263c061ca60258859a970c
Juan Antonio <ja.sanchezmartel@gmail.com>
2025-10-31 17:27:31 +0000

    Created Hospital seeder

---
+++ src/main/resources/db/migration/V4__seed_hospital.sql
@@ -0,0 +1,12 @@
+INSERT INTO Hospital (
+    cif,
+    name,
+    address,
+    email,
+    phoneNumber,
+    password
+)
+VALUES
+('HOSP12345A', 'Hospital General de Madrid', 'Calle Mayor 123, Madrid', 'info@hospitalmadrid.com', '911234567', '$2a$10$exampleHash1'),
+('HOSP67890B', 'Clínica Vida Salud', 'Avenida Andalucía 45, Sevilla', 'contacto@vidasalud.com', '954678901', '$2a$10$exampleHash2'),
+('HOSP54321C', 'Hospital del Norte', 'Calle Aragón 89, Barcelona', 'info@hospitalnorte.com', '934567890', '$2a$10$exampleHash3');
