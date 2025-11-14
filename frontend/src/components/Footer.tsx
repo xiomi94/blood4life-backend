aa053f8792ac66a013fcd2b26a5ed55ae0753c1e
jasanchez97 <ja.sanchezmartel@gmail.com>
2025-11-14 19:43:02 +0000

    Improving CRUD Hospital Issue 101

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <div className="flex flex-row w-full fixed bottom-0 left-0 bg-red-200">
+      <div className="flex flex-row w-full bottom-0 left-0 bg-red-200">
         <div className="w-full"/>
         <div className="content-center">
           Información
